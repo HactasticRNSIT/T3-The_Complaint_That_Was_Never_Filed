dc746cec8477521e7db366a25eb39499c2da29ed
AditiSinha285 <aditisinha2807@gmail.com>
2026-05-08 13:00:21 +0530

    Adding Emergency sos button

---
+++ EmergencySOS/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from urllib.parse import quote
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/send_sos", methods=["POST"])
+def send_sos():
+    data = request.json
+    location = data["location"]
+
+    phone = "918310022396"  # must be exact format
+
+    message = quote(f"🚨 EMERGENCY ALERT! Location: {location}")
+
+    whatsapp_link = f"https://wa.me/{phone}?text={message}"
+
+    print("DEBUG LINK:", whatsapp_link)  # IMPORTANT for testing
+
+    return jsonify({"whatsapp_link": whatsapp_link})
+
+if __name__ == "__main__":
+    app.run(debug=True)
